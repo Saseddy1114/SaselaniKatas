754187b94fd9095a3282976dade8cb5ae5c62adb
Saseddy1114 <116673212+Saseddy1114@users.noreply.github.com>
2023-07-26 16:58:55 +0200

    Update Copier.cs

--- CharacterCopier/CharacterCopier/Copier.cs
+++ CharacterCopier/CharacterCopier/Copier.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,13 +17,21 @@ namespace CharacterCopier
             _destination = destination;
         }
 
-        public void Copy()
+        public void Copy(int? numberOfCharacters = null)
         {
             char c;
-
-            while (( c = _source.ReadChar()) != '\n')
+            if (numberOfCharacters != null)
             {
-                _destination.WriteChar(c);
+                int index = _source.ReadChars(numberOfCharacters.GetValueOrDefault(0)).ToList().IndexOf('\n');
+                index = index == -1 ? numberOfCharacters.GetValueOrDefault(0) : index;
+                _destination.WriteChars(_source.ReadChars(index - 1));
+             }
+            else
+            { 
+                while ((c = _source.ReadChar()) != '\n')
+                {
+                    _destination.WriteChar(c);  
+                }
             }
         }
 
